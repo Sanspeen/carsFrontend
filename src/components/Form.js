1d89851faabbc73e71b64ecd1c8240b2aab3a049
Santiago Franco <ktns0930@gmail.com>
2021-09-14 18:03:26 -0500

    Revert "A little visual modificatios has been added."
    This reverts commit caa2616f24aec3837bbd58785150ec816668910a.

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,7 +18,7 @@ const Form = () => {
     return ( 
         <Fragment >
             <center><h1>INGRESE LOS DATOS DE SU VEHICULO.</h1></center><br></br>
-            <form className = "col-sm" onSubmit = {handleSubmit(onSubmit)} border: 2px>
+            <form className = "col-sm" onSubmit = {handleSubmit(onSubmit)}>
 
                 <input
                     type = "text"
